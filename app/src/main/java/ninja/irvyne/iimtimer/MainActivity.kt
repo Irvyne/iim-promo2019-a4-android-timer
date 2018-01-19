655b5c77502e71e355b6e083e47d5882795edd60
Thibaud BARDIN <thibaud.bardin@gmail.com>
2018-01-19 12:31:46 +0100

    😍

--- app/src/main/java/ninja/irvyne/iimtimer/MainActivity.kt
+++ app/src/main/java/ninja/irvyne/iimtimer/MainActivity.kt
@@ -2,11 +2,33 @@ package ninja.irvyne.iimtimer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.d(TAG, "progress: " + progress.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStartTrackingTouch")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                Log.d(TAG, "onStopTrackingTouch")
+            }
+
+        })
+    }
+
+    companion object {
+        internal const val TAG = "MainActivity"
     }
 }
